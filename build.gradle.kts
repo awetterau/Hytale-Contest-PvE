4148093a6a0a91753075add9e5aa0f839d211364
darlak97 <darlak97games@gmail.com>
2026-04-27 00:05:10 -0700

    Crimson and audio updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,50 +1,27 @@
-/**
- * NOTE: This is entirely optional and basics can be done in `settings.gradle.kts`
- */
+plugins {
+    id("java")
+}
+
+group = "dev.hytalemodding"
+version = "1.0-SNAPSHOT"
 
 repositories {
-    // Any external repositories besides: MavenLocal, MavenCentral, HytaleMaven, and CurseMaven
+    mavenCentral()
 }
 
 dependencies {
-    // Any external dependency you also want to include
-    implementation("com.hypixel.hytale:Server:2026.03.26-89796e57b")
+    compileOnly(files("libs/HytaleServer.jar"))
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-val devModsRoot = layout.buildDirectory.dir("devmods")
-val devModPackDir = layout.buildDirectory.dir("devmods/ExamplePlugin")
-
-val prepareDevModLayout by tasks.registering(Copy::class) {
-    into(devModPackDir)
-    from("src/main/resources/Server") {
-        into("Server")
-    }
-    if (file("src/main/resources/Common").exists()) {
-        from("src/main/resources/Common") {
-            into("Common")
-        }
-    }
-    if (file("src/main/resources/manifest.json").exists()) {
-        from("src/main/resources/manifest.json")
-    }
+tasks.test {
+    useJUnitPlatform()
 }
 
-tasks.named<JavaExec>("runServer") {
-    dependsOn(prepareDevModLayout)
-    doFirst {
-        val modsPath = devModsRoot.get().asFile.absolutePath
-        val current = args?.toMutableList() ?: mutableListOf()
-        var replaced = false
-        for (i in current.indices) {
-            if (current[i].startsWith("--mods=")) {
-                current[i] = "--mods=$modsPath"
-                replaced = true
-            }
-        }
-        if (!replaced) {
-            current.add("--mods=$modsPath")
-        }
-        setArgs(current)
-        println("runServer effective mods path: $modsPath")
-    }
+tasks.jar {
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    destinationDirectory.set(file("D:\\Program Files\\Hytale\\UserData\\Mods"))
+
+    from("src/main/resources")
 }
